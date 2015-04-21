31635b68de2f3f97d3ac97c7fd2ba3fb70dc52cf
Javier <javierhonduco@gmail.com>
2015-04-21 13:10:20 +0200

    Added twitter import

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'twitter'
 require 'dotenv'
 Dotenv.load
 
@@ -29,9 +30,10 @@ class AsyncProcessor
   def process tweet
     # do nothing for now
     # should insert the normalized tweet in the db
+    puts tweet
   end
 end
 
-# config = {consumer_key: ENV["CONSUMER_KEY"], consumer_secret: ENV["CONSUMER_SECRET"], access_token: ENV["ACCESS_TOKEN"], access_token_secret: ENV[ "ACCESS_SECRET"]}
-#geo = GeoStreamer.new config
-#geo.track_keywords 'a, b'
+config = {consumer_key: ENV["CONSUMER_KEY"], consumer_secret: ENV["CONSUMER_SECRET"], access_token: ENV["ACCESS_TOKEN"], access_token_secret: ENV[ "ACCESS_SECRET"]}
+geo = GeoStreamer.new config
+geo.track_keywords 'a, b'
